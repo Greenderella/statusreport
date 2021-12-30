d278d06ecfabf8011fdfd9ee9446224fba7a42b0
Alejandra Bañay <abanay@bitwarden.com>
2021-12-30 12:31:42 +0100

    secret

--- status.py
+++ status.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 status = 0
 params = {'status': status}
-r = requests.post('https://bitwardentest.hund.io/state_webhook/watchdog/61cd8a01386fb37c3d04b049', headers = {"X-WEBHOOK-KEY": os.environ.get('X-WEBHOOK-KEY')}, params=params)
+r = requests.post('https://bitwardentest.hund.io/state_webhook/watchdog/61cd8a01386fb37c3d04b049', headers = {"X-WEBHOOK-KEY": os.environ.get('X_WEBHOOK_KEY')}, params=params)
 
 r.raise_for_status()
